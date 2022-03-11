2570e84901483630c52beca103471bf43592f3e1
JeffoWoo <webdevjeff@msn.com>
2022-03-11 17:48:34 +0000

    Initial web app

---
+++ src/pages/HomePage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const  HomePage = () => (
+    <>
+        <h1>Hello, Welcome To My Blog!</h1>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in feugiat velit, ut dapibus justo. Donec vitae orci odio. Pellentesque sagittis mi elit, at convallis diam laoreet ac. Nulla congue orci non eros eleifend venenatis. Nam ut bibendum lectus. Praesent vitae tristique nibh, eu dapibus tortor. Maecenas sit amet dui arcu. Vivamus vel hendrerit lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+        </p>
+        <p>
+        Proin semper tempus finibus. Duis eu gravida tortor. Praesent ac nisl tempus, scelerisque nisl ut, venenatis lacus. Maecenas mattis enim eu sem molestie, in pellentesque libero tristique. Nulla ac leo nec lacus aliquam faucibus ac eu elit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin vel dignissim felis.  
+        </p>
+        <p>
+        Fusce condimentum ex elit, quis tempus nunc lacinia a. Ut aliquam tempor euismod. Duis ultrices pulvinar ex. Vivamus lacinia id felis ut pulvinar. Integer quis arcu a magna pretium ultrices. Suspendisse commodo nec turpis at rutrum. Phasellus mattis lacinia fermentum. Duis finibus aliquet convallis. Vivamus posuere convallis nulla, id iaculis arcu bibendum a. Cras nec augue et nisi venenatis malesuada. In hac habitasse platea dictumst. Pellentesque aliquam elit ut lacus varius, non pulvinar velit volutpat. Quisque rhoncus sit amet velit sed mollis. 
+        </p>
+    </>
+    );
+
+
+export default HomePage;
